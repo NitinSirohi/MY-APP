400f6f42c54776f5a3ca92b4bd6fc2ca800bdc20
Nitin Sirohi <nitinsirohi927@gmail.com>
2019-09-15 22:15:14 +0530

    Contact Info Page using Reactjs

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Aboutstyle.css';
+const About = () => {
+    return(
+        <div className="aboutstyle">
+            <h2>Welcome to about us page.</h2>
+
+        </div>
+    )
+}
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Contactstyle.css';
+import Form from './Form';
+const Contact= () => {
+    return(
+        <div className="Contactstyle">
+            
+            
+            <br/>
+            
+             <Form/>
+        </div>
+    );
+}
+export default Contact;
